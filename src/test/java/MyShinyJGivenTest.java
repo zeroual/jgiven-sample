769fdb325faefa531351d15ae6b4d45636c50263
zeroual <abid1991@hotmail.fr>
2017-06-02 17:29:44 +0200

    Use Junit rules

--- src/test/java/MyShinyJGivenTest.java
+++ src/test/java/MyShinyJGivenTest.java
@@ -1,15 +1,33 @@
 
-import com.tngtech.jgiven.junit.ScenarioTest;
+import com.tngtech.jgiven.annotation.ScenarioStage;
+import com.tngtech.jgiven.junit.JGivenClassRule;
+import com.tngtech.jgiven.junit.JGivenMethodRule;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
-public class MyShinyJGivenTest
-        extends ScenarioTest<GivenSomeState, WhenSomeAction, ThenSomeOutcome> {
+public class MyShinyJGivenTest {
+
+    @ClassRule
+    public static final JGivenClassRule writerRule = new JGivenClassRule();
+
+    @Rule
+    public final JGivenMethodRule scenarioRule = new JGivenMethodRule();
+
+    @ScenarioStage
+    GivenSomeState someState;
+
+    @ScenarioStage
+    WhenSomeAction someAction;
+
+    @ScenarioStage
+    ThenSomeOutcome someOutcome;
 
     @Test
     public void something_should_happen() {
-        given().some_state();
-        when().some_action();
-        then().some_outcome();
+        someState.given().some_state();
+        someAction.when().some_action();
+        someOutcome.then().some_outcome();
     }
 }
 
